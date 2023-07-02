e5cc9ea351025d12002b00747c8c481be042e7a3
Dan Sheffner <dan@sheffner.com>
2023-07-02 06:43:56 -0500

    adding a script to reset vm

---
+++ reset_vm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "reset_vm started..."
+echo "destroying vm..."
+terraform destroy -auto-approve > destroy-vm.log
+echo "sleeping 10 seconds..."
+sleep 10
+echo "creating vm..."
+terraform apply -auto-approve > create-vm.log
+echo "sleeping 30 seconds for cloud-init to finish..."
+sleep 30
+echo "reset_vm completed."
