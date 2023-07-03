cf566e9bfdf0f5304b962647c1c81d23db2296a5
Dan Sheffner <thesheff17@gmail.com>
2023-07-03 16:27:59 -0500

    made injecting the public key option.

--- create_vm_9000.sh
+++ create_vm_9000.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# install libguestfs-tools on your proxmox server
-# sudo apt update -y && sudo apt install libguestfs-tools -y
-
 echo "ceate_vm_9000.sh started..."
 
 if [ "$EUID" -ne 0 ]
@@ -10,17 +7,19 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-FILE1=/root/ansible_ssh_key.txt
-if test -f "$FILE1"; then
-    echo "found ansible ssh key file..."
-else
-    echo "could not find /root/anible_ssh_key.txt file.  Please create.  exiting."
-    exit
-fi
+# example if you need to inject a key into the image
+# FILE1=/root/ansible_ssh_key.txt
+# if test -f "$FILE1"; then
+#    echo "found ansible ssh key file..."
+#  else
+#    echo "could not find /root/anible_ssh_key.txt file.  Please create this file. exiting."
+#    exit
+# fi
 
-FILE2=/root/jammy-server-cloudimg-amd64.img
+FILE2=/root/jammy-server-cloudimg-amd64.img.original
 if test -f "$FILE2"; then
      echo "found img file skipping download..."
+     cp /root/jammy-server-cloudimg-amd64.img.original =/root/jammy-server-cloudimg-amd64.img
 else
      echo "downloading img file..."
      cd /root/
@@ -30,7 +29,7 @@ fi
 virt-customize -a jammy-server-cloudimg-amd64.img --install qemu-guest-agent
 virt-customize -a jammy-server-cloudimg-amd64.img --run-command "useradd -m -s /bin/bash ubuntu"
 virt-customize -a jammy-server-cloudimg-amd64.img --root-password password:ubuntu
-virt-customize -a jammy-server-cloudimg-amd64.img --ssh-inject ubuntu:file:/root/ansible_ssh_key.txt
+# virt-customize -a jammy-server-cloudimg-amd64.img --ssh-inject ubuntu:file:/root/ansible_ssh_key.txt
 qm create 9000 --memory 2048 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci
 qm set 9000 --scsi0 local-lvm:0,import-from=/root/jammy-server-cloudimg-amd64.img
 qm set 9000 --ide2 local-lvm:cloudinit
